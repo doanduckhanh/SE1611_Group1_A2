e15ea039a8c1fb97e0ba937c9cd10b193703ec1b
thanhnhhe153751 <thanknguyen2662@gmail.com>
2023-02-13 23:26:52 +0700

    test

--- SE1611_Group1_A2/SE1611_Group1_A2/obj/Debug/net6.0-windows/App.g.i.cs
+++ SE1611_Group1_A2/SE1611_Group1_A2/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace SE1611_Group1_A2 {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace SE1611_Group1_A2 {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
         public static void Main() {
             SE1611_Group1_A2.App app = new SE1611_Group1_A2.App();
             app.InitializeComponent();
